5441eb2a6fbb3bda0084ec6a48a3ce96a68020a1
Sanuja Methmal <methmal66@yahoo.com>
2021-02-13 21:13:47 +0530

    modify docker configs

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const port = 3000;
+const port = 5000;
 
 app.use(bodyParser.json());
 app.use("/api/users", require("./routes/user"));
