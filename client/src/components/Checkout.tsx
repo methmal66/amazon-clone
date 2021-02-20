f17f92842bf4b72493dc23611ad86143fe4185d9
Sanuja Methmal <methmal66@yahoo.com>
2021-02-20 15:57:56 +0530

    add features to header

--- client/src/components/Checkout.tsx
+++ client/src/components/Checkout.tsx
@@ -1,17 +1,7 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import Header from "./Header";
 
 class Checkout extends Component {
-  static instance:Checkout;
-
-  constructor(props:any) {
-    if (!Checkout.instance) {
-      super(props)
-      Checkout.instance = this;
-    }
-    return Checkout.instance;
-  }
-
   render() {
     return (
       <div>
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,38 +1,52 @@
 import React, { Component } from "react";
-import Grid from "@material-ui/core/Grid";
 import { Link } from "react-router-dom";
 import logo from "./logo.png";
+import { Icon } from "@iconify/react";
+import bxsUser from "@iconify-icons/bx/bxs-user";
+import bsxCart from "@iconify-icons/bx/bxs-cart";
+import searchOutlined from "@iconify-icons/ant-design/search-outlined"
 
 class Header extends Component {
-  static instance: Header;
-
-  constructor(props: any) {
-    if (!Header.instance) {
-      super(props);
-      Header.instance = this;
-    }
-    return Header.instance;
-  }
-
   render() {
-    const amazonLogo = (
-      <Link to="/">
-        <img
-          className="Header_image"
-          src={logo}
-          alt="amazon logo"
-        />
-      </Link>
-    );
-
     return (
       <div className="Header_root">
-        <Grid container>
-          <Grid item>{amazonLogo}</Grid>
-        </Grid>
+        {this.amazonLogo}
+        {this.search}
+        {this.buttons}
       </div>
     );
   }
+
+  amazonLogo = (
+    <Link to="/">
+      <img className="Header_image" src={logo} alt="amazon logo" />
+    </Link>
+  );
+
+  search = (
+    <div className="Header_search">
+      <input type="text" className="Header_searchbar" />
+      <div className="Header_searchIconHolder">
+        <Icon className="Header_icon" icon={searchOutlined} />
+      </div>
+    </div>
+  );
+
+  buttons = (
+    <div>
+      <a href="/login">
+        <button className="Header_btn">
+          <Icon className="Header_icon" icon={bxsUser} />
+        </button>
+      </a>
+      <a href="/checkout">
+        <button className="Header_btn">
+          <Icon className="Header_icon" icon={bsxCart} />
+        </button>
+      </a>
+
+    </div>
+  );
 }
 
 export default Header;
